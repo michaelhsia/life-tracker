d54cb9b2645d0fc559f36000963a168d231e1b76
Michael Hsia <michaelhsia8716@gmail.com>
2024-04-20 17:29:33 +0800

    feat: add primevue, header and update css

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <ul>
+      <li><a href="#">首頁</a></li>
+      <li><a href="#">旅遊地點</a></li>
+      <li><a href="#">推薦遊戲</a></li>
+    </ul>
+  </div>
+</template>
+
+
+
+<style>
+ul {
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
+import PrimeVue from 'primevue/config';
+import Menubar from 'primevue/menubar';
+
+import './assets/style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue)
+app.component('Menubar', Menubar)
+app.mount('#app')
+
+
