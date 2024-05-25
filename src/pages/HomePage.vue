495b3670694900b484fa0c62f6ce40d014ed8feb
Michael Hsia <michaelhsia8716@gmail.com>
2024-05-25 16:12:11 +0800

    fix: add primeflex

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,29 +1,115 @@
 <template>
-  <div class="container m0-auto">
-    <MeterGroup :value="value" :max="200" />
-    <Card style="width: 400px; overflow: hidden">
-      <template #header>
-        <img style="height: 350px; object-fit: cover;" alt="user header"
-          src="https://plus.unsplash.com/premium_photo-1715876234432-aee502a59dcd?q=80&w=1200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-      </template>
-      <template #title>Advanced Card</template>
-      <template #subtitle>Card subtitle</template>
-      <template #content>
-        <p class="m-0">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam
-          deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate
-          neque
-          quas!
-        </p>
-      </template>
-      <template #footer>
-        <div class="flex gap-3 mt-1">
-          <Button label="Cancel" severity="secondary" outlined class="w-full" />
-          <Button label="Save" class="w-full" />
-        </div>
-      </template>
-    </Card>
+  <div class="grid">
+    <div class="col-12">
+      <MeterGroup class="mb-2" :value="value" :max="200" />
+    </div>
 
+  </div>
+  <div class="grid">
+    <div class="col-12 md:col-6 lg:col-3">
+      <Card style="overflow: hidden">
+        <template #header>
+          <img style="width: 100%; height: 250px; object-fit: cover;" alt="user header"
+            src="https://plus.unsplash.com/premium_photo-1715876234432-aee502a59dcd?q=80&w=1200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        </template>
+        <template #title>Advanced Card</template>
+        <template #subtitle>Card subtitle</template>
+        <template #content>
+          <p class="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
+            numquam
+            deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse,
+            cupiditate
+            neque
+            quas!
+          </p>
+        </template>
+        <template #footer>
+          <div class="flex gap-3 mt-1">
+            <Button label="Cancel" severity="secondary" outlined class="w-full" />
+            <Button label="Save" class="w-full" />
+          </div>
+        </template>
+      </Card>
+    </div>
+    <div class="col-12 md:col-6 lg:col-3">
+      <Card style="overflow: hidden">
+        <template #header>
+          <img style="width: 100%; height: 250px; object-fit: cover;" alt="user header"
+            src="https://plus.unsplash.com/premium_photo-1715876234432-aee502a59dcd?q=80&w=1200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        </template>
+        <template #title>Advanced Card</template>
+        <template #subtitle>Card subtitle</template>
+        <template #content>
+          <p class="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
+            numquam
+            deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse,
+            cupiditate
+            neque
+            quas!
+          </p>
+        </template>
+        <template #footer>
+          <div class="flex gap-3 mt-1">
+            <Button label="Cancel" severity="secondary" outlined class="w-full" />
+            <Button label="Save" class="w-full" />
+          </div>
+        </template>
+      </Card>
+    </div>
+    <div class="col-12 md:col-6 lg:col-3">
+      <Card style="overflow: hidden">
+        <template #header>
+          <img style="width: 100%; height: 250px; object-fit: cover;" alt="user header"
+            src="https://plus.unsplash.com/premium_photo-1715876234432-aee502a59dcd?q=80&w=1200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        </template>
+        <template #title>Advanced Card</template>
+        <template #subtitle>Card subtitle</template>
+        <template #content>
+          <p class="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
+            numquam
+            deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse,
+            cupiditate
+            neque
+            quas!
+          </p>
+        </template>
+        <template #footer>
+          <div class="flex gap-3 mt-1">
+            <Button label="Cancel" severity="secondary" outlined class="w-full" />
+            <Button label="Save" class="w-full" />
+          </div>
+        </template>
+      </Card>
+    </div>
+    <div class="col-12 md:col-6 lg:col-3">
+      <Card style="overflow: hidden">
+        <template #header>
+          <img style="width: 100%; height: 250px; object-fit: cover;" alt="user header"
+            src="https://plus.unsplash.com/premium_photo-1715876234432-aee502a59dcd?q=80&w=1200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        </template>
+        <template #title>Advanced Card</template>
+        <template #subtitle>Card subtitle</template>
+        <template #content>
+          <p class="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
+            numquam
+            deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse,
+            cupiditate
+            neque
+            quas!
+          </p>
+        </template>
+        <template #footer>
+          <div class="flex gap-3 mt-1">
+            <Button label="Cancel" severity="secondary" outlined class="w-full" />
+            <Button label="Save" class="w-full" />
+          </div>
+        </template>
+      </Card>
+    </div>
   </div>
 </template>
 
